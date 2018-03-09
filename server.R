32caf4ed991e6b3f30eb77ccf5fd037978dc1297
Diogo S. B. Rocha <diogosbr@gmail.com>
2018-03-09 18:35:38 -0300

    adicionando uma interface Shiny
    A interface foi desenvolvida por Renata Capellão

---
+++ server.R
@@ -0,0 +1,64 @@
+
+diversidade <- function(x, table = F, ntable = "diversity",  base = exp(1)) {
+  if ("vegan" %in% rownames(installed.packages())) {
+    cat("Pacotes OK!")
+  } else {
+    (install.packages("vegan", dependencies = T))
+  }
+  
+  if ("xlsx" %in% rownames(installed.packages())) {
+    cat("Pacotes OK!\n")
+  } else {
+    (install.packages("xlsx"))
+  }
+  require(vegan)
+  require(xlsx)
+  
+  final <- data.frame(matrix(nrow = length(row.names(x)), ncol = 4))
+  colnames(final) <- c("Shannon (H')", "Equabilidade (J')", "Espécies (S)", "Abundância")
+  row.names(final) <- row.names(x)
+  
+  final[, 1] <- diversity(x, base = base)
+  final[, 2] <- diversity(x, base = base) / log(specnumber(x), base = base)
+  final[, 3] <- specnumber(x)
+  final[, 4] <- rowSums(x)
+  if (table == T) {
+    write.xlsx(final, paste0(ntable, ".xlsx"))
+  }
+  return(final)
+}
+
+
+
+function(input, output) {
+  
+  observeEvent(input$btn_calcular, {
+    df <- read.table(
+      input$file1$datapath,
+      header = input$header,
+      sep = input$sep,
+      quote = input$quote,
+      row.names = 1)
+    
+    disp <- input$disp
+    
+    base <- input$base
+    
+    resultado <- diversidade(x = df, base = as.numeric(base))
+    
+    output$input_data <- renderTable({
+      if (disp == "head") {
+        return(head(df))
+      }
+      else {
+        return(df)
+      }
+    })
+    
+    output$results <- renderTable({
+      resultado
+    })
+  })
+}
+
+# Create Shiny app
---
+++ ui.R
@@ -0,0 +1,86 @@
+library(shiny)
+
+# Define UI for data upload app ----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Diversidade"),
+  helpText("Cálculo dos índices de diversidade de espécies"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Select a file ----
+      fileInput("file1", "Importar arquivo .csv",
+        multiple = TRUE,
+        accept = c("text/csv",
+          "text/comma-separated-values,text/plain",
+          ".csv")),
+      
+      # Horizontal line ----
+      tags$hr(),
+      
+      # Input: Checkbox if file has header ----
+      checkboxInput("header", "Cabeçalho", TRUE),
+      
+      # Input: Select separator ----
+      radioButtons("sep", "Separador",
+        choices = c("Vígula" = ",",
+          "Ponto e vírgula" = ";",
+          "Tabulação" = "\t"),
+        selected = ","),
+      
+      # Input: Select quotes ----
+      radioButtons("quote", "Definição de texto:",
+        choices = c("Ausente" = "",
+          "Aspas duplas" = '"',
+          "Aspas simples" = "'"),
+        selected = '"'),
+      
+      # Horizontal line ----
+      tags$hr(),
+      
+      # Input: Select number of rows to display ----
+      radioButtons("disp", "Modo de exibição",
+        choices = c("Mostrar somente as primeiras linhas" = "head",
+          "Mostrar todos os registros" = "all"),
+        selected = "head"),
+      
+      
+      # Horizontal line ----
+      tags$hr(),
+      
+      radioButtons("base", "Base de logaritmo usada no índice de Shannon",
+        choices = c("Bits" = "2",
+          "Nats" = "exp(1)",
+          "Decits" = "10")
+        
+      ) ,
+      actionButton("btn_calcular", "Calcular!")
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      tabsetPanel(
+        tabPanel("Entrada de dados", 
+          
+          tableOutput("input_data")
+          
+        ),
+        tabPanel("Resultado",
+          tableOutput("results")
+        )
+        
+      )
+      
+    )
+    
+    
+  )
+  
+  
+)
